5b0968f391fee6ea4bb4b6a832448bea6dbc3425
Vitaly <answer-0885@mail.ru>
2022-10-13 16:48:41 +0700

    Лекция 34

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -4,14 +4,19 @@ import Card from "../UI/Card";
 
 
 const CostItem = (props) => {
-
+    let description = props.description;
+    const changeDescriptionHandler = ()=>{
+        description = "New text"
+        console.log(description);
+    }
     return(
         <Card className="cost-item">
             <CostDate date={props.date}/>
             <div className="cost-item__descripton">
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
                 <div className="cost-item__price">${props.amount}</div>
             </div>
+            <button onClick={changeDescriptionHandler}>Изменить описание</button>
         </Card>
     )
 };

6b977d62ddde723d8ea86d8026e0e30d0e79f939
Vitaly <answer-0885@mail.ru>
2022-10-20 10:31:41 +0700

    42 Лекция

---
+++ src/components/NewCost/NewCost.js
@@ -0,0 +1,8 @@
+import CostForm from './CostForm'
+import './NewCost.css';
+const NewCost = () =>{
+ return <div className="new-cost">
+            <CostForm/>
+       </div>
+}
+export default NewCost;

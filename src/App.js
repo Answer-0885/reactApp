2e608918e9518ac44660823b8120be4b650b148a
Vitaly <answer-0885@mail.ru>
2022-11-07 14:50:08 +0700

    Лекция 50

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-// import React from "react";
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
+import React,{useState} from "react";
+
+const INITIAL_COSTS = [
+  {
+    id:'c1',
+    date: new Date(2022, 4, 1),
+    description: "Холодильник",
+    amount: "999.99"
+  },
+  {
+    id:'c2',
+    date: new Date(2022, 7, 5),
+    description: "Notebook",
+    amount: "699.99"
+  },
+  {
+    id:'c3',
+    date: new Date(2022, 9, 7),
+    description: "Зимние ботинки",
+    amount: "299.99"
+  }
+];
 
 const App = () => {
 
-  const costs = [
-    {
-      id:'c1',
-      date: new Date(2022, 4, 1),
-      description: "Холодильник",
-      amount: "999.99"
-    },
-    {
-      id:'c2',
-      date: new Date(2022, 7, 5),
-      description: "Notebook",
-      amount: "699.99"
-    },
-    {
-      id:'c3',
-      date: new Date(2022, 9, 7),
-      description: "Зимние ботинки",
-      amount: "299.99"
-    }
-  ];
+ const[costs, setCosts] = useState(INITIAL_COSTS)
 // return React.createElement(
 //   "div",
 //   {},
@@ -32,8 +34,9 @@ const App = () => {
 // );
 
   const addCostHandler = (cost) =>{
-    console.log(cost);
-    console.log("App Component");
+    setCosts(prevCosts =>{
+      return [cost, ...prevCosts]
+    });
   }
 
   return (
